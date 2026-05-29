a2f683e4da69dbc2d0824a64b118d9e1c18377b0
git-Ar-Ch <167996016+git-Ar-Ch@users.noreply.github.com>
2026-05-29 09:16:12 -0400

    Add files via upload

---
+++ RockPaperScissors.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+public class RockPaperScissors {
+	static int determineWinner (ArrayList<String> choices) {
+		if (choices.contains("R") && choices.contains("S")) {
+			return choices.indexOf("R");
+		}
+		if (choices.contains("R") && choices.contains("P")) {
+			return choices.indexOf("P");
+		}
+		if (choices.contains("P") && choices.contains("S")) {
+			return choices.indexOf("S");
+		}
+		return -1;
+	}
+	public static void main(String[] args) {
+		
+	}
+}
